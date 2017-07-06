318e5b6bf10f25154d4aae2d66de280c13b72559
CK Luna <ckmluna@gmail.com>
2017-07-06 15:42:49 +0800

    - Uses PhantomJS instead of Firefox. - Fixed deployment script for staging.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 server "40.117.91.189", port: 22, roles: [:app], :primary => true, user: 'deployer'
 set :application => "dev-newsbyte-scrapper"
 set :branch, "staging"
-set :deploy_to, "/newsbyte_dev"
+set :deploy_to, "/dev.newsbyte"
